71d7bd4af059e15908c0429c9033c1be5c32a11a
Adwithya Magow <a.magow@chaostheory.hk>
2023-04-17 17:28:12 +0800

    Format mappings

--- mappings.lua
+++ mappings.lua
@@ -2,21 +2,27 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+	},
+}
+
+M.nvimtree = {
+	n = {
+		["<C-f>"] = { "<cmd> NvimTreeFindFile <CR>", "find file nvimtree" },
+	},
 }
 
 M.clipboard = {
-  n = {
-    ["<leader>y"] = { '"+y', "Copy to clipboard" },
-    ["<leader>p"] = { '"+p', "Paste from clipboard" },
-  },
+	n = {
+		["<leader>y"] = { '"+y', "Copy to clipboard" },
+		["<leader>p"] = { '"+p', "Paste from clipboard" },
+	},
 }
 
 M.fugitive = {
-  n = {
-    ["<leader>gg"] = { "<Cmd>Git<CR>", "Open fugitive" },
-  },
+	n = {
+		["<leader>gg"] = { "<Cmd>Git<CR>", "Open fugitive" },
+	},
 }
 return M
