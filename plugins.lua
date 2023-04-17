b61ebebd713932cd5df9fdfdb3793b04001a7e21
Adwithya Magow <a.magow@chaostheory.hk>
2023-04-17 17:43:45 +0800

    Attempt setup copilot with config

--- plugins.lua
+++ plugins.lua
@@ -80,41 +80,7 @@ local plugins = {
 			},
 		},
 		opts = function()
-			local opts = require("plugins.configs.cmp")
-			local cmp = require("cmp")
-
-			local has_words_before = function()
-				if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-					return false
-				end
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-			end
-
-			-- add copilot source
-			opts.sources.insert("name", "copilot")
-			-- update sorting
-			opts.sorting = {
-
-				priority_weight = 2,
-				comparators = {
-					require("copilot_cmp.comparators").prioritize,
-
-					-- Below is the default comparator list and order for nvim-cmp
-					cmp.config.compare.offset,
-					-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
-					cmp.config.compare.exact,
-					cmp.config.compare.score,
-					cmp.config.compare.recently_used,
-					cmp.config.compare.locality,
-					cmp.config.compare.kind,
-					cmp.config.compare.sort_text,
-					cmp.config.compare.length,
-					cmp.config.compare.order,
-				},
-			}
-			return opts
+			return require("custom.configs.cmp")
 		end,
 	},
 }
