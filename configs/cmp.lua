0fb5179e6b2aef788e4e40d760c08109fbcfeba8
Adwithya Magow <a.magow@chaostheory.hk>
2023-04-17 17:54:18 +0800

    Add mapping for all keys

--- configs/cmp.lua
+++ configs/cmp.lua
@@ -20,6 +20,16 @@ table.insert(opts.sources, { name = "copilot" })
 
 -- update mapping
 opts.mapping = {
+	["<C-p>"] = cmp.mapping.select_prev_item(),
+	["<C-n>"] = cmp.mapping.select_next_item(),
+	["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	["<C-f>"] = cmp.mapping.scroll_docs(4),
+	["<C-Space>"] = cmp.mapping.complete(),
+	["<C-e>"] = cmp.mapping.close(),
+	["<CR>"] = cmp.mapping.confirm({
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	}),
 	["<Tab>"] = cmp.mapping(function(fallback)
 		if cmp.visible() and has_words_before() then
 			cmp.select_next_item()
