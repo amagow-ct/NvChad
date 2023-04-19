e3727e7f71e07850ccf51b9fb959e4ead6bd045c
Adwithya Magow <a.magow@chaostheory.hk>
2023-04-19 11:48:05 +0800

    feat: do not use clangd for proto

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,27 +1,38 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
-  "html",
-  "cssls",
-  "tsserver",
-  "clangd",
-  "gopls",
-  "dockerls",
-  "bufls",
-  "bashls",
-  "rust_analyzer",
-  "sqlls",
+	"html",
+	"cssls",
+	"tsserver",
+	"clangd",
+	"gopls",
+	"dockerls",
+	"bufls",
+	"bashls",
+	"rust_analyzer",
+	"sqlls",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	if lsp == "clangd" then
+		lspconfig[lsp].setup({
+			filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+			on_attach = on_attach,
+			capabilities = capabilities,
+		})
+		goto continue
+	end
+
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+
+	::continue::
 end
 
 --
