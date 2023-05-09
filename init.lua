1b1787f5f99835f3264b32e457fc03939e7466fd
Adwithya Magow <a.magow@chaostheory.hk>
2023-05-09 14:52:31 +0800

    feat: setup code folding using treesitter

--- init.lua
+++ init.lua
@@ -3,36 +3,39 @@ local api = vim.api
 local opt = vim.opt
 
 opt.relativenumber = true
+opt.foldenable = false
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- Does not work unless is a callback
 local foldCallback = function()
-	vim.schedule(function()
-		vim.cmd("normal zR")
-	end)
+  vim.schedule(function()
+    vim.cmd("normal zR")
+  end)
 end
 local treesitterFoldGroup = api.nvim_create_augroup("TreesitterOpenFolds", { clear = true })
 api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
-	pattern = "*",
-	group = treesitterFoldGroup,
-	callback = foldCallback,
+  pattern = "*",
+  group = treesitterFoldGroup,
+  callback = foldCallback,
 })
 
 local autoformatGroup = api.nvim_create_augroup("Autoformat", { clear = true })
 local setNoAutoFmtCallback = function()
-	vim.b.noAutoFmt = true
+  vim.b.noAutoFmt = true
 end
 api.nvim_create_autocmd("FileType", {
-	pattern = "yaml",
-	group = autoformatGroup,
-	callback = setNoAutoFmtCallback,
+  pattern = "yaml",
+  group = autoformatGroup,
+  callback = setNoAutoFmtCallback,
 })
 local autoFmtCallback = function()
-	if not vim.b.noAutoFmt then
-		vim.lsp.buf.format()
-	end
+  if not vim.b.noAutoFmt then
+    vim.lsp.buf.format()
+  end
 end
 api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	group = autoformatGroup,
-	callback = autoFmtCallback,
+  pattern = "*",
+  group = autoformatGroup,
+  callback = autoFmtCallback,
 })
