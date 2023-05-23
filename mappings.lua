255db52c26f7cd76e98d5211d54262a583ac0446
Adwithya Magow <a.magow@chaostheory.hk>
2023-05-23 17:03:10 +0800

    feat: add sql formatting

--- mappings.lua
+++ mappings.lua
@@ -2,68 +2,68 @@
 local M = {}
 
 M.general = {
-	n = {
-		[";"] = { ":", "enter command mode", opts = { nowait = true } },
-	},
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
 }
 
 M.nvimtree = {
-	n = {
-		["<C-f>"] = { "<cmd> NvimTreeFindFile <CR>", "find file nvimtree" },
-	},
+  n = {
+    ["<C-f>"] = { "<cmd> NvimTreeFindFile <CR>", "find file nvimtree" },
+  },
 }
 
 M.clipboard = {
-	n = {
-		["<leader>y"] = { '"+y', "Copy to clipboard" },
-		["<leader>p"] = { '"+p', "Paste from clipboard" },
-	},
+  n = {
+    ["<leader>y"] = { '"+y', "Copy to clipboard" },
+    ["<leader>p"] = { '"+p', "Paste from clipboard" },
+  },
 }
 
 M.fugitive = {
-	n = {
-		["<leader>gg"] = { "<Cmd>Git<CR>", "Open fugitive" },
-	},
+  n = {
+    ["<leader>gg"] = { "<Cmd>Git<CR>", "Open fugitive" },
+  },
 }
 
 M.copilot = {
-	n = {
-		["<leader>po"] = { "<Cmd>Copilot panel<CR>", "Open panel copilot" },
-		["<leader>pa"] = {
-			function()
-				require("copilot.panel").accept()
-			end,
-			"accept suggestion copilot",
-		},
-		["<leader>pn"] = {
-			function()
-				require("copilot.panel").jump_next()
-			end,
-			"Next suggestion copilot",
-		},
-		["<leader>pp"] = {
-			function()
-				require("copilot.panel").jump_prev()
-			end,
-			"Previous suggestion copilot",
-		},
-		["<leader>pr"] = {
-			function()
-				require("copilot.panel").refresh()
-			end,
-			"Refresh panel copilot",
-		},
-	},
+  n = {
+    ["<leader>po"] = { "<Cmd>Copilot panel<CR>", "Open panel copilot" },
+    ["<leader>pa"] = {
+      function()
+        require("copilot.panel").accept()
+      end,
+      "accept suggestion copilot",
+    },
+    ["<leader>pn"] = {
+      function()
+        require("copilot.panel").jump_next()
+      end,
+      "Next suggestion copilot",
+    },
+    ["<leader>pp"] = {
+      function()
+        require("copilot.panel").jump_prev()
+      end,
+      "Previous suggestion copilot",
+    },
+    ["<leader>pr"] = {
+      function()
+        require("copilot.panel").refresh()
+      end,
+      "Refresh panel copilot",
+    },
+  },
 }
 
 M.telescope = {
-	n = {
-		["<leader>fw"] = {
-			function()
-				require("telescope").extensions.live_grep_args.live_grep_args()
-			end,
-			"live grep",
-		},
-	},
+  n = {
+    ["<leader>fw"] = {
+      function()
+        require("telescope").extensions.live_grep_args.live_grep_args()
+      end,
+      "live grep",
+    },
+  },
 }
 return M
